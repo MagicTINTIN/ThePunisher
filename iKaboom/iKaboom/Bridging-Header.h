b4a8c84c78c1b5446acfec4b161c3826dd0332f2
Valentin Servieres <vservieres@laas.fr>
2025-08-28 11:23:33 +0200

    chore: linking lib with the app

---
+++ iKaboom/iKaboom/Bridging-Header.h
@@ -0,0 +1,2 @@
+#include "../../libfun/libfun.h"
+
--- iKaboom/iKaboom/ContentView.swift
+++ iKaboom/iKaboom/ContentView.swift
@@ -1,24 +1,35 @@
-//
-//  ContentView.swift
-//  iKaboom
-//
-//  Created by temp on 28/08/2025.
-//
-
 import SwiftUI
+import UniformTypeIdentifiers
+#if os(macOS)
+import AppKit
+#endif
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var isRunning: Bool = false
 
-#Preview {
-    ContentView()
+    var body: some View {
+        VStack(spacing: 12) {
+            Form {
+                HStack {
+                    Button(action: runFun) {
+                        Text(isRunning ? "Generating fun..." : "Press here to have fun")
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .background(Color.red)
+                    .cornerRadius(2)
+                    .padding()
+                    .disabled(isRunning)
+                }
+            }
+            .padding()
+        }
+    }
+
+    private func runFun() {
+
+        isRunning = true
+        havefun()
+        isRunning = false
+    }
 }
