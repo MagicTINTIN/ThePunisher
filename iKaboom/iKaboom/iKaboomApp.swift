5d9f4f2c6290519506ce82e671e59951fd5ab0b5
Valentin Servieres <vservieres@laas.fr>
2025-08-28 10:26:17 +0200

    feat: iKaboom shitty apple application initialisation

---
+++ iKaboom/iKaboom/iKaboomApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKaboomApp.swift
+//  iKaboom
+//
+//  Created by temp on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct iKaboomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
