04efa594255efa2280f60a78169b643bbad9b97a
Valentin Servieres <vservieres@laas.fr>
2025-08-28 15:51:30 +0200

    feat: new app, iPaf
    deepseek ce génie

---
+++ iPaf/iPaf/iPafApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPafApp.swift
+//  iPaf
+//
+//  Created by temp on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct iPafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
